95e59596cc6e4d3e3d90c0285f64b4044cf5289d
Rushikesh Ghuge <rushikeshghuge993@gmail.com>
2023-08-22 16:00:35 +0530

    pushiT

--- StudentmanagmentSystem/main.js
+++ StudentmanagmentSystem/main.js
@@ -1,78 +1,97 @@
-let addBtn = document.getElementById('add');
-let list = [];
-addBtn.addEventListener('click', () => {
-    let obj = {
-        id: list.length + 1,
-        name: document.getElementById('name').value,
-        email: document.getElementById('email').value,
-        gap: document.getElementById('gpa').value,
-        age: document.getElementById('age').value,
-        degree: document.getElementById('degree').value
+const students = [
+    {
+        ID: 1,
+        name: 'Alice',
+        age: 21,
+        grade: 'A',
+        degree: 'Btech',
+        email: 'alice@example.com'
+    },
+    {
+        ID: 2,
+        name: 'Bob',
+        age: 22,
+        grade: 'B',
+        degree: 'MBA',
+        email: 'bob@example.com'
+    },
+    {
+        ID: 3,
+        name: 'Charlie',
+        age: 20,
+        grade: 'C',
+        degree: 'Arts',
+        email: 'charlie@example.com'
     }
-    appendInTable(obj)
-    list.push(obj)
-    let form = document.getElementById('form');
-    for (let i = 1; i < 6; i++) {
-        form[i - 1].value = '';
-    }
-})
+];
 
-let tBody = document.getElementById('tBody');
-function appendInTable(obj) {
-    let tr = document.createElement('tr');
-    for (let i = 0; i < 6; i++) {
+
+// (1)=> Render Student Function :
+const tBody = document.getElementById('table-body')
+let renderStudent = () => {
+    tBody.innerHTML = ''; // make to clear first
+    students.forEach((student) => {
+        let trow = document.createElement('tr');
+        const cols = ['ID', 'name', 'email', 'grade', 'age', 'degree'];
+        cols.forEach(col => {
+            let td = document.createElement('td');
+            td.textContent = student[col];
+            trow.appendChild(td)
+        })
         let td = document.createElement('td');
-        td.innerText = Object.values(obj)[i];
-        tr.appendChild(td);
-        if (i == 5) {
-            td.innerHTML = `<p>${Object.values(obj)[i]}</p> <span><i class="fa-solid fa-pen-to-square update"></i> <i id="dele" class="fa-solid fa-trash"></i></span>`
-            td.className = 'delAndSelect'
-        }
-    }
-    tBody.appendChild(tr)
-    // delete that 
-    let del = document.getElementsByClassName('fa-trash');
-    for (let i = 0; i < del.length; i++) {
-        del[i].addEventListener('click', (event) => {
-            event.target.parentNode.parentNode.parentNode.remove();
-        })
-    }
 
-    let update = document.querySelectorAll('.update');
-    for (let i = 0; i < update.length; i++) {
-        update[i].addEventListener('click', (event) => {
-            let info = update[i].parentNode.parentNode.parentNode;
-            console.log(info.children);
-            forUpdateInput(info.children)
-        })
-    }
+        let delBtn = document.createElement('button');
+        delBtn.innerText = 'Delete'
+        delBtn.onclick=(() => deleteStudednt(student.ID))
 
-}
+        let editBtn = document.createElement('button');
+        editBtn.innerText = 'Edit'
+        editBtn.onclick=(() => editStudent(student.ID))
 
-
-
-function forUpdateInput(info) {
-    let form = document.getElementById('form');
-
-    for (let i = 1; i < 6; i++) {
-        form[i - 1].value = info[i].innerText;
-    }
-
-    let updateBtn = document.getElementById('updateBtn');
-    updateBtn.addEventListener('click', () => {
-        console.log(form);
-        for (let i = 1; i < 6; i++) {
-            info[i].innerText = form[i - 1].value
-            if (i == 5) {
-                info[i].innerHTML = `<p>${form[i - 1].value}</p> <span><i class="fa-solid fa-pen-to-square update"></i> <i id="dele" class="fa-solid fa-trash"></i></span>`
-            }
-        }
-        //delete listner add
-        
-        // clear the input data 
-        for (let i = 1; i < 6; i++) {
-            form[i - 1].value = '';
-        }
+        td.append(delBtn, editBtn)
+        trow.appendChild(td)
+        tBody.appendChild(trow)
     })
 }
+renderStudent();
 
+// (2)=> add Student Function :
+let addStudent = ()=>{
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let grade = document.getElementById('grade').value;
+    let age = document.getElementById('age').value;
+    let degree = document.getElementById('degree').value;
+    let StudentId = document.getElementById('student-id');
+    console.log(StudentId);
+    let newStudent={
+        ID:students.length+1,
+        name:name,
+        email:email,
+        grade:grade,
+        age:age,
+        degree:degree,
+    }
+    students.push(newStudent)
+    renderStudent();
+    return false
+}   
+
+
+// (3)=> update student Function:
+
+function editStudent(studentId){
+    let student = students.find((s)=>s.ID == studentId)
+    console.log(student);
+    document.getElementById('name').value = student.name
+    document.getElementById('age').value = student.age
+    document.getElementById('grade').value = student.grade
+    document.getElementById('email').value = student.email
+    document.getElementById('degree').value = student.degree
+    document.getElementById('student-id').value = student.ID
+    document.getElementById('sub-btn').innerText = 'Update Student'
+}
+
+
+// (4)=> Delete Student Function:
+// (5)=> Search Student Function 
